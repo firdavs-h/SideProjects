f34db69c64acd1f3e845c00d187c037169081cd4
Firdavs Hasanov <firdavs_h2@outlook.com>
2020-10-06 12:09:18 -0400

    Added some changes to the code

--- searchWords/src/main/java/side/projects/SearchWords.java
+++ searchWords/src/main/java/side/projects/SearchWords.java
@@ -16,10 +16,10 @@ public class SearchWords {
 	private static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		System.out.println("******************");
-		System.out.println("    Word Search");
-		System.out.println("******************");
+
+		System.out.println("======================");
+		System.out.println("   Search A Word In The File");
+		System.out.println("======================");
 
 		File file = verifiedInputFile();
 		String word = textInput();
@@ -36,11 +36,11 @@ public class SearchWords {
 			int lineCount = 0;
 			while (fileScanner.hasNextLine()) {
 
-				if (caseSensitive) { //case sensitive search
+				if (caseSensitive) { // case sensitive search
 					String line = fileScanner.nextLine();
 					lineCount++;
 
-					if (line.contains(" "+searchWord)) {
+					if (line.contains(searchWord)) {
 						System.out.println(lineCount + ") " + line);
 					}
 
@@ -48,7 +48,7 @@ public class SearchWords {
 					String line = fileScanner.nextLine().toLowerCase();
 					lineCount++;
 
-					if (line.contains(" "+searchWord.toLowerCase())) {
+					if (line.contains(searchWord.toLowerCase())) {
 						System.out.println(lineCount + ") " + line);
 
 					}
@@ -58,7 +58,7 @@ public class SearchWords {
 	}
 
 	public static File fileInput() {
-		System.out.println("What is the file that should be searched? ");
+		System.out.print("Name / Locate the file that should be searched? >>> ");
 
 		String filePath = input.nextLine();
 		File inputFile = new File(filePath);
@@ -69,15 +69,15 @@ public class SearchWords {
 	public static File verifiedInputFile() {
 
 		File temp = fileInput();
-		//checks if file valid or exists
+		// checks if file valid or exists
 		while (temp.exists() != true || temp.isFile() != true) {
 			if (temp.exists() == false) {
-				System.out.println(temp.getPath() + " file does not exist, please try again");
-				temp = fileInput();     //prompts new input from user, if file does not exists
-
-			} else if (temp.isFile() == false) {
-				System.out.println(temp.getPath() + " is not a file, please try again");
-				temp = fileInput();   //prompts new input from user, if not a file
+				System.out.println(temp.getAbsolutePath() + " file does not exist, please try again");
+				temp = fileInput(); // prompts new input from user, if file does not exists
+				if (temp.isFile() == false) {
+					System.out.println(temp.getAbsolutePath() + " is not a file, please try again");
+					temp = fileInput(); // prompts new input from user, if not a file
+				}
 
 			}
 		}
@@ -87,7 +87,7 @@ public class SearchWords {
 	}
 
 	public static String textInput() {
-		System.out.println("What is the search word you are looking for?");
+		System.out.print("Type a word / phase part of the word to search >>> ");
 		String lookWord = input.nextLine();
 
 		return lookWord;
@@ -97,14 +97,14 @@ public class SearchWords {
 	public static boolean caseSensitive() {
 		System.out.println("Should the search be case sensitive? (Y\\N)");
 		String caseInput = input.nextLine().toUpperCase();
-		
-		//checks if Y or N entered
+
+		// checks if Y or N entered
 		while (!caseInput.equals("Y") && !caseInput.equals("N")) {
 			System.out.println(caseInput + " entered, please enter Y or N");
-			caseInput = input.nextLine().toUpperCase(); // if not  Y or N prompts new entry
+			caseInput = input.nextLine().toUpperCase(); // if not Y or N prompts new entry
 
 		}
-		input.close();
+		input.close(); // closing scanner in last method that uses it
 		return caseInput.equals("Y");
 
 	}
